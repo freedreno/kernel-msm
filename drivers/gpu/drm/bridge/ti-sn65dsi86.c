428fb3dec0677f5661c741d3b3f57dd445beda12
Rob Clark <robdclark@chromium.org>
2019-06-29 11:44:50 -0700

    drm/bridge: ti-sn65dsi86: support booloader enabled display
    Request the enable gpio ASIS to avoid disabling bridge during probe, if
    already enabled.  And if already enabled, defer enabling runpm until
    attach to avoid cutting off the power to the bridge.
    
    Once we get to attach, we know panel and drm driver are probed
    successfully, so at this point it is safe to enable runpm and reset the
    bridge.  If we do it earlier, we kill efifb (in the case that panel or
    drm driver do not probe successfully), giving the user no way to see
    what is going on.
    
    Signed-off-by: Rob Clark <robdclark@chromium.org>

--- drivers/gpu/drm/bridge/ti-sn65dsi86.c
+++ drivers/gpu/drm/bridge/ti-sn65dsi86.c
@@ -377,6 +377,12 @@ static int ti_sn_bridge_attach(struct drm_bridge *bridge,
 		return -EINVAL;
 	}
 
+	if (gpiod_get_value(pdata->enable_gpio)) {
+		pm_runtime_enable(pdata->dev);
+		ti_sn_bridge_resume(pdata->dev);
+		ti_sn_bridge_suspend(pdata->dev);
+	}
+
 	ret = drm_connector_init(bridge->dev, &pdata->connector,
 				 &ti_sn_bridge_connector_funcs,
 				 DRM_MODE_CONNECTOR_eDP);
@@ -1409,6 +1415,7 @@ static int ti_sn_bridge_probe(struct i2c_client *client,
 			      const struct i2c_device_id *id)
 {
 	struct ti_sn_bridge *pdata;
+	bool boot_on = false;
 	int ret;
 
 	if (!i2c_check_functionality(client->adapter, I2C_FUNC_I2C)) {
@@ -1440,7 +1447,7 @@ static int ti_sn_bridge_probe(struct i2c_client *client,
 	dev_set_drvdata(&client->dev, pdata);
 
 	pdata->enable_gpio = devm_gpiod_get(pdata->dev, "enable",
-					    GPIOD_OUT_LOW);
+					    GPIOD_ASIS);
 	if (IS_ERR(pdata->enable_gpio)) {
 		DRM_ERROR("failed to get enable gpio from DT\n");
 		ret = PTR_ERR(pdata->enable_gpio);
@@ -1468,17 +1475,23 @@ static int ti_sn_bridge_probe(struct i2c_client *client,
 	if (ret)
 		return ret;
 
-	pm_runtime_enable(pdata->dev);
+	boot_on = gpiod_get_value(pdata->enable_gpio);
+
+	if (!boot_on) {
+		pm_runtime_enable(pdata->dev);
+	}
 
 	ret = ti_sn_setup_gpio_controller(pdata);
 	if (ret) {
-		pm_runtime_disable(pdata->dev);
+		if (!boot_on)
+			pm_runtime_disable(pdata->dev);
 		return ret;
 	}
 
 	ret = ti_sn_setup_pwmchip(pdata);
 	if (ret)  {
-		pm_runtime_disable(pdata->dev);
+		if (!boot_on)
+			pm_runtime_disable(pdata->dev);
 		return ret;
 	}
 
