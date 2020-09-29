a8d9a53ad423d41bc0623e737714df671767a459
Rob Clark <robdclark@chromium.org>
2020-09-29 15:11:32 -0700

    WIP: drm/msm/dpu: delete some code

--- drivers/gpu/drm/msm/disp/dpu1/dpu_encoder.c
+++ drivers/gpu/drm/msm/disp/dpu1/dpu_encoder.c
@@ -1736,27 +1736,6 @@ static void dpu_encoder_vsync_event_handler(struct timer_list *t)
 {
 	struct dpu_encoder_virt *dpu_enc = from_timer(dpu_enc, t,
 			vsync_event_timer);
-	struct drm_encoder *drm_enc = &dpu_enc->base;
-	struct msm_drm_private *priv;
-	struct msm_drm_thread *event_thread;
-
-	if (!drm_enc->dev || !drm_enc->crtc) {
-		DPU_ERROR("invalid parameters\n");
-		return;
-	}
-
-	priv = drm_enc->dev->dev_private;
-
-	if (drm_enc->crtc->index >= ARRAY_SIZE(priv->event_thread)) {
-		DPU_ERROR("invalid crtc index\n");
-		return;
-	}
-	event_thread = &priv->event_thread[drm_enc->crtc->index];
-	if (!event_thread) {
-		DPU_ERROR("event_thread not found for crtc:%d\n",
-				drm_enc->crtc->index);
-		return;
-	}
 
 	del_timer(&dpu_enc->vsync_event_timer);
 }
